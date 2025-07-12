5a938ac2c5e675caf6eafa5f3183bd539c467ce4
teaqu <teaqu@outlook.com>
2025-07-12 18:13:21 +0100

    better lines

--- shadertoy/squares7.glsl
+++ shadertoy/squares7.glsl
@@ -6,7 +6,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   vec2 uv = fragCoord / iResolution.xy;
   vec2 uGrid = vec2(uv.x * gx, uv.y * gy);
   vec2 grid = floor(uGrid);
-  vec2 gb = step(0.01, fract(uGrid));
+  vec2 gb = smoothstep(0.01, 0.03, fract(uGrid));
   vec3 col = vec3(grid.x / gx, grid.y / gy, 1.0);
   fragColor = vec4(col * gb.x * gb.y, 1.0);
 }
